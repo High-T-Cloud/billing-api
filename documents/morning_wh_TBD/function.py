29a8ed9ab7a1a2631191b6271f849d56f744ef89
high-t-cloud <tomer@high-t.cloud>
2023-07-05 11:15:05 +0300

    Added authentication to morning webhook function

--- documents/morning_wh_TBD/function.py
+++ documents/morning_wh_TBD/function.py
@@ -1,6 +1,30 @@
+import hmac
+import hashlib
 import requests
 import os
 import utils
+import boto3
+import json
+
+
+def authenticate(body, signature):
+    """
+    Use hmac to validate webhook message legitimacy.
+    """
+    print('--verifying message authenticity-- ')
+    # Get the hmac secret key from secretsmanager
+    sm_client = boto3.client('secretsmanager')
+    res = sm_client.get_secret_value(SecretId=os.environ['MORNING_SECRET_ARN'])
+    secret_value = json.loads(res['SecretString'])
+    
+    hmac_secret = secret_value['webhook_secret']
+    print('--retrieved secret valuest--')
+    
+    digest = hmac.new(key=hmac_secret.encode('utf-8'), msg=body.encode('utf-8'), digestmod=hashlib.sha256).hexdigest()
+    verified = hmac.compare_digest(digest, signature)
+    print('--verified to: ', verified)
+    return verified
+    
 
 # TODO: implement
 def handle_non_invoice():
@@ -16,26 +40,35 @@ def lambda_handler(event, context):
     """
     proccess messages coming from morning webhook (validation and auth is done before this step)
     """
+    
     print('--event: ', event)
+    
+    # TODO: Altert on false message!
+    if not authenticate(event['body'], event['headers']['X-Webhook-Signature']):
+        print('--FOUND NON LEGITIMATE MESSAGE--')
+        raise Exception('err-403: Message was not found as legitimate')
+    
     INVOICE_TYPES = [320]  # documents types that need to be handeled
     PROFORMA_TYPE = 300
+    
+    body = json.loads(event['body'])
 
     # Check the document type
-    if int(event['type']) not in INVOICE_TYPES:
+    if int(body['type']) not in INVOICE_TYPES:
         return handle_non_invoice()
 
     # --Document is relevant--
 
     # Call morning api to get linked documents
     print('--calling morning api to get linked documents--')
-    url = f'{os.environ["MORNING_URL"]}/{event["id"]}/linked'
+    url = f'{os.environ["MORNING_URL"]}/{body["id"]}/linked'
     token = utils.get_morning_token(os.environ['MORNING_SECRET_ARN'])
     headers = {'Authorization': 'Bearer ' + token}
 
     res = requests.get(url, headers=headers)
     print('--response status code: ', res.status_code)
     if res.status_code < 200 or res.status_code > 299:
-        # TODO: pay attention - severe error
+        # TODO: pay attention - severe error!
         print('--morning response: ', res)
         raise Exception('err-500: unable to get linked documents')        
 
@@ -73,7 +106,7 @@ def lambda_handler(event, context):
     # TODO: handle mixed currencies between invoice and proforma
 
     # Update the amount and delete the document if needed
-    new_proforma_amount = proforma_db['value'] - float(event['total'])
+    new_proforma_amount = proforma_db['value'] - float(body['total'])
     print('--new amount from profroma: ', new_proforma_amount)
     if new_proforma_amount <= 0:
         print('--removing proforma from DB--')
@@ -85,6 +118,3 @@ def lambda_handler(event, context):
     conn.commit()
 
     return 0
-
-    
-
